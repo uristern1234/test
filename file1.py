68a072ccb603507c0c94c850e6cfd16f60edf6d9
uri stern <uri@uris-MacBook-Pro.local>
2024-08-01 16:18:22 +0300

    removing old files

--- file1.py
+++
@@ -1,7 +0,0 @@
-import utils
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding
-
-def Sign(self, data):
-        return self.rsa_key.sign(
-            data, padding.PKCS1v15(), utils.Prehashed(hashes.SHA1()))
